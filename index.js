ba86407f65af9ce7581410c85e8ec8de0be7fef4
KrystynaMil <kristina-manager@hotmail.com>
2021-02-24 16:30:26 +0100

    added get a single course by id  from courses.json

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-
-// FRAMEWORKS
+// FRAMEWORK
 
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 
 app.use(express.json());
+
+
+// READ courses.json file
+const coursesPatch = __dirname + '/courses.json';
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Group1');
     }); 
 
-// READ courses.json file
-const COURSES_PATH = __dirname + '/courses.json';
+// GET a single course by id from courses.json
 
+app.get('/courses.json/:id', (req, res) => {
+    fs.readFile(coursesPatch, 'utf-8', (err, courses) => {
+      if (err) {
+        return console.error(err);
+      }
+  
+      const allCourses = JSON.parse(courses);
+  
+      const course = allCourses.find((c) => c.id === parseInt(req.params.id));
+      if (!course) return res.status(404).send('The course with the given ID was not found');
+  
+      res.send(course);
+    });
+  });
 
 
     //PORT
 const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
