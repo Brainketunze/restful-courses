31ceea0f4ccc2797198366e9a676fabf349979f9
KrystynaMil <kristina-manager@hotmail.com>
2021-02-24 17:06:14 +0100

    added to index.js get all courses

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-// FRAMEWORKS
+// FRAMEWORK
 
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 
 app.use(express.json());
 
@@ -12,8 +12,34 @@ app.get('/', (req, res) => {
     }); 
 
 // READ courses.json file
-const COURSES_PATH = __dirname + '/courses.json';
+const coursesPatch = __dirname + '/courses.json';
 
+// GET a single course by id from courses.json
+
+app.get('/courses.json/:id', (req, res) => {
+    fs.readFile(coursesPatch, 'utf-8', (err, courses) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      const allCourses = JSON.parse(courses);
+
+      const course = allCourses.find((c) => c.id === parseInt(req.params.id));
+      if (!course) return res.status(404).send('The course with the given ID was not found');
+
+      res.send(course);
+    });
+  });
+// GET all courses 
+app.get('/courses.json',  (req, res) => {
+    fs.readFile(coursesPatch, 'utf-8', (err, courses) => {
+      if (err) {
+        console.error(err);
+      }
+      JSON.parse(courses);
+      res.send(courses);
+    });
+  });
 
 
     //PORT
