743f1f2e3525d276635752bd22c7bd0c16fdf03b
KrystynaMil <kristina-manager@hotmail.com>
2021-02-24 15:24:48 +0100

    added index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// FRAMEWORKS
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from Group1');
+    }); 
+
+// READ courses.json file
+const COURSES_PATH = __dirname + '/courses.json';
+
+
+
+    //PORT
+const port = process.env.PORT || 3000; 
+app.listen(port, () => console.log(`Listening on port ${port}...`));
