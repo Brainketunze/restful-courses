050bdfc0a167bbedc8cc45d3cac0fd7da6d6f617
Anisya <67062698+AnisyaPurnama@users.noreply.github.com>
2021-02-25 10:47:36 +0100

    app-put

--- index.js
+++ index.js
@@ -45,6 +45,35 @@ app.get('/courses.json',  (req, res) => {
     });
   });
 
+//UPDATE or EDIT
+app.put('courses.json/:id', (req, res) => {
+  fs.readFile(coursesPatch, 'utf-8', (err, courses) => {
+    if (err) {
+      return console.error(err);
+    }
+
+  const course = allCourses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found');//404 (object not found)
+
+  //validate
+  //If invalid, return 400 - Bad request
+  const { error } = validateCourse(req.body); // result.error
+  if (error) return res.status(400).send(error.details[0].message);
+  
+
+  //Update course
+  course.name = req.body.name;
+  res.send(course); //Return the update course
+  });
+});
+
+function validateCourse(course) {
+  const schema = {
+      name:Joi.string().min(3).required() 
+  };
+  return Joi.validate(course, schema);
+}
+
 //PORT
 const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
